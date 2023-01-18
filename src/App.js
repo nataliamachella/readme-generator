9c0386c3880aee1e4c65f1cf27dc282fc196ab88
nataliamachella <nataliamachella@gmail.com>
2023-01-18 13:25:10 -0300

    agregué el header y estilos a la app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useState } from "react";
+import logoPreview from "./assets/Icon-preview.png";
+import logoCopy from "./assets/Icon-copy.png";
 
 function App() {
   const [form, setForm] = useState({});
@@ -12,48 +14,73 @@ function App() {
   };
 
   return (
-    <div className="contenedor">
-      <div className="formulario">
+    <>
+      <header className="header">
         <h1>README Generator</h1>
-        <form>
-          <label>Título</label>
-          <input value={form.title} name="title" onChange={handleChange} />
+      </header>
+      <div className="contenedor">
+        <div className="formulario">
+          <form>
+            <div>
+              <label>Título</label>
+              <input value={form.title} name="title" onChange={handleChange} />
+            </div>
 
-          <label>Descripción</label>
-          <textarea
-            value={form.description}
-            name="description"
-            onChange={handleChange}
+            <div>
+              <label>Descripción</label>
+              <textarea
+                value={form.description}
+                name="description"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label>Imagen de Portada</label>
+              <input value={form.image} name="image" onChange={handleChange} />
+            </div>
+            <div>
+              <label>Stack Tecnológico</label>
+              <input value={form.stack} name="stack" onChange={handleChange} />
+            </div>
+            <div>
+              <label>Licencia</label>
+              <input
+                value={form.license}
+                name="license"
+                onChange={handleChange}
+              />
+            </div>
+          </form>
+        </div>
+        {/*  CÓDIGO */}
+        <div className="code">
+          <div className="code__icon">
+            <img src={logoCopy} />
+          </div>
+          <p>
+            # {form.title} <br />
+            {form.description} <br />
+            ![App Screenshot]({form.image})
+          </p>
+        </div>
+
+        {/*  VISTA PREVIA */}
+        <div className="prev">
+          <div className="prev__icon">
+            <img src={logoPreview} />
+          </div>
+          <h1>{form.title}</h1>
+
+          <p>{form.description}</p>
+          <img
+            src="https://via.placeholder.com/468x300?text=App+Screenshot"
+            height={300}
+            width={430}
           />
-
-          <label>Imagen de Portada</label>
-          <input value={form.image} name="image" onChange={handleChange} />
-
-          <label>Herramientas</label>
-          <input value={form.stack} name="stack" onChange={handleChange} />
-
-          <label>Licencia</label>
-          <input value={form.license} name="license" onChange={handleChange} />
-        </form>
+          <h2>Stack Tecnológico</h2>
+        </div>
       </div>
-
-      <div className="prev">
-        <h1>{form.title}</h1>
-        <hr />
-
-        <p>{form.description}</p>
-        <img src="https://via.placeholder.com/468x300?text=App+Screenshot+Here" />
-        <h2>Stack Tecnológico</h2>
-        <hr />
-      </div>
-      <div className="code">
-        <p>
-          # {form.title} <br />
-          {form.description} <br />
-          ![App Screenshot]({form.image})
-        </p>
-      </div>
-    </div>
+    </>
   );
 }
 
